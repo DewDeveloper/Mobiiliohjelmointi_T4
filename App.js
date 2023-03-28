50ba9a16cbd021205d3cf243445234446118a8d1
Matias Karila <matias.karila@myy.haaga-helia.fi>
2023-03-28 16:30:24 +0300

    Tehtävä valmis

--- App.js
+++ App.js
@@ -1,11 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, FlatList, StyleSheet, Text, TextComponent, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [item, setItem] = useState("");
+  const [items, setItems] = useState([]);
+
+  const addItem = () => {
+    setItems([...items, {key: item}]);
+  }
+
+  const clearItems = () => {
+    setItems([]);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <Text></Text>
+      <Text></Text>
+      <Text></Text>
+      <Text></Text>
+
+      <TextInput
+        style={styles.input}
+        onChangeText={setItem}
+        keyboardType="default" 
+        underlineColorAndroid={"blue"}/>
+
+      <Text></Text>
+      <Text></Text>
+      <Text></Text>
+
+      <View style={styles.row}>
+        <Button
+            style={styles.button}
+            title="ADD"
+            color="blue"
+            onPress={addItem} />
+        <Button
+            style={styles.button}
+            title="CLEAR"
+            color="blue"
+            onPress={clearItems} />
+      </View>
+
+      <Text></Text>
+      <Text></Text>
+      
+      <Text style={styles.blue}>Shopping list</Text>
+
+      <FlatList
+        data={items}
+        renderItem={({item}) => (
+          <Text>{item.key}</Text>
+          )
+        }
+      />
     </View>
   );
 }
@@ -16,5 +68,24 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 30,
+    width: 150,
+    margin: 5,
+    borderWidth: 2,
+    padding: 5,
+  },
+  button: {
+    height: 20,
+    width: 20,
+    justifyContent: "center",
+    padding: 150,
+  },
+  row: {
+    flexDirection: "row",
+  },
+  blue: {
+    color: "blue",
   },
 });
